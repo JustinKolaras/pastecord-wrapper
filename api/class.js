5b69724fc93990ca54a25b9cc14af1331d4ca054
Justin K <justinkolaras@outlook.com>
2022-03-12 10:25:32 -0800

    Init

---
+++ api/class.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+
+const REQUEST_URL = "https://pastecord.com/documents";
+const DOC_URL = "https://pastecord.com/";
+
+module.exports = class PastecordAPI {
+    async publish(body) {
+        let result;
+        try {
+            result = await axios.post(REQUEST_URL, body);
+        } catch (err) {
+            throw new Error(err);
+        }
+        return DOC_URL + result.data.key;
+    }
+};
